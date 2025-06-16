dea5f759f24339d50ae470ad2a01960b7bd06bf0
macarena <macarena.lani@gmail.com>
2025-06-16 03:26:02 -0300

    Modif

--- src/app/pages/turnos/turnos.component.ts
+++ src/app/pages/turnos/turnos.component.ts
@@ -32,64 +32,97 @@ export class TurnosComponent {
   listaComentarios: any[] = [];
 
   // variables de la barra de busqueda
-  busquedaTexto : string = "";
+  busquedaTexto : string = '';
   todosTurnos: boolean = false;
   mensajeEstadoB : string = "";
 
   constructor()
   {
     this.db.listarTurnos().then((turnos: any[])=>{
-      this.todosTurnos = true;
-      this.listaTurnosCompleta = turnos;
-      this.listaTurnos = this.listaTurnosCompleta;
+      // this.todosTurnos = true;
+      // this.listaTurnosCompleta = turnos;
+      this.listaTurnos = turnos;
+      this.listaEncontrados = this.listaTurnos;
+      console.log(this.listaEncontrados);
     });  
   }
 
   ngOnInit()
   {
-    if(this.todosTurnos == true)
-    {
-      this.listaTurnos = this.listaTurnosCompleta;
-    }
-    else
-    {
-      this.listaTurnos = this.listaEncontrados;
-    }
+    this.listaEncontrados = this.listaTurnos;
+    // this.buscar();
+    // if(this.todosTurnos == true)
+    // {
+    //   this.listaTurnos = this.listaTurnosCompleta;
+    // }
+    // else
+    // {
+    //   this.listaTurnos = this.listaEncontrados;
+    // }
   }
-  
-  buscar()
+
+  filtrarTurno()
   {
-    if(this.busquedaTexto != "")
+    const texto = this.busquedaTexto.toLowerCase().trim();
+
+    if(texto == '')
     {
-
-      for(let i = 0; i < this.listaTurnos.length; i++)
-      {
-        if(this.busquedaTexto.toLowerCase() == this.listaTurnos[i].especialista.nombre.toLowerCase() || (this.busquedaTexto.toLowerCase() == this.listaTurnos[i].especialidad.toLowerCase()))
-        {
-          this.listaEncontrados.push(this.listaTurnos[i]);
-        }
-      }
-
-      if(this.listaEncontrados.length > 0)
-      {
-        if(this.listaEncontrados.length == 1)
-        {
-          this.mensajeEstadoB = "Hay " + this.listaEncontrados.length + " coincidencia";
-        }
-        else
-        {
-          this.mensajeEstadoB = "Hay " + this.listaEncontrados.length + " coincidencias";          
-        }
-        this.todosTurnos = false;
-        this.ngOnInit();
-      }
-      else
-      {
-        this.mensajeEstadoB = "No hay ningun especialista o especialidad con ese nombre";
-        this.listaTurnos = [];
-      }
+      this.listaEncontrados = [...this.listaTurnos];
+      return;
     }
+
+    this.listaEncontrados = this.listaTurnos.filter(turno => {
+      return (
+        String(turno.fecha).toLowerCase().includes(texto) ||
+        String(turno.especialista.nombre).toLowerCase().includes(texto) ||
+        String(turno.especialidad).toLocaleLowerCase().includes(texto) ||
+        String(turno.paciente.nombre).toLowerCase().includes(texto) ||
+        String(turno.paciente.ObraSocial).toLowerCase().includes(texto) ||
+        String(turno.estado).toLowerCase().includes(texto)
+      );
+    });
+
   }
+
+  // onTextoCambio(valor: string)
+  // {
+  //   this.busquedaTexto = valor; 
+  //   console.log(valor);
+  // }
+  
+  // buscar()
+  // {
+  //   if(this.busquedaTexto != "")
+  //   {
+
+  //     for(let i = 0; i < this.listaTurnos.length; i++)
+  //     {
+  //       if(this.busquedaTexto.toLowerCase() == this.listaTurnos[i].especialista.nombre.toLowerCase() || (this.busquedaTexto.toLowerCase() == this.listaTurnos[i].especialidad.toLowerCase()))
+  //       {
+  //         this.listaEncontrados.push(this.listaTurnos[i]);
+  //       }
+  //     }
+
+  //     if(this.listaEncontrados.length > 0)
+  //     {
+  //       if(this.listaEncontrados.length == 1)
+  //       {
+  //         this.mensajeEstadoB = "Hay " + this.listaEncontrados.length + " coincidencia";
+  //       }
+  //       else
+  //       {
+  //         this.mensajeEstadoB = "Hay " + this.listaEncontrados.length + " coincidencias";          
+  //       }
+  //       this.todosTurnos = false;
+  //       this.ngOnInit();
+  //     }
+  //     else
+  //     {
+  //       this.mensajeEstadoB = "No hay ningun especialista o especialidad con ese nombre";
+  //       this.listaTurnos = [];
+  //     }
+  //   }
+  // }
 
   cambiarEstado(estadoNuevo: string,idTurno : number = 0)
   {
